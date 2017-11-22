209bd3d494e6e38b78bf86a5cacc68c79844f93a
Anouk ZOUAOUI <anzouaoui78@yahoo.fr>
2017-11-22 14:42:38 +0100

    Modification de l'écran de base en fullscreen

--- app/src/main/java/com/itescia/compagnysimulator/MainActivity.java
+++ app/src/main/java/com/itescia/compagnysimulator/MainActivity.java
@@ -1,7 +1,9 @@
 package com.itescia.compagnysimulator;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 }
