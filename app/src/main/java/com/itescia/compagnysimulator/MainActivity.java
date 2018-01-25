118b1304ab420e2433711f7aee420ee8dab63324
Anouk ZOUAOUI <anouk.zouaoui@anoukzouaoui.fr>
2018-01-25 09:39:51 +0100

    Création des progressBar personnalisées
    Modifications des progressBars en progressBar personnalisées.

--- app/src/main/java/com/itescia/compagnysimulator/MainActivity.java
+++ app/src/main/java/com/itescia/compagnysimulator/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.itescia.compagnysimulator.Employes.Commercial;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     TextView textViewLevel, textViewArgent, textViewHomme, textViewAddRessources;
     Typeface typefaceLevel, typefaceRessource;
+    ProgressBar progressBarReputation, progressBarSecurite, progressBarFormation, progressBarBonheur, progressBarRessources;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,14 +31,17 @@ public class MainActivity extends AppCompatActivity {
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
-        initialze();
+        initialize();
     }
 
-    private void initialze() {
+    /**
+     * Fonction permettant d'initialiser les éléments du graphique principale
+     */
+    private void initialize() {
+        //ELEMENTS TEXTES
         textViewLevel = (TextView) findViewById(R.id.TextViewLevel);
         typefaceLevel = Typeface.createFromAsset(getAssets(), "font/fipps_regular.ttf");
         textViewLevel.setTypeface(typefaceLevel);
-
         textViewArgent = (TextView) findViewById(R.id.TextViewArgent);
         textViewHomme = (TextView) findViewById(R.id.TextViewHomme);
         textViewAddRessources = (TextView) findViewById(R.id.TextViewAddRessources);
@@ -43,5 +49,17 @@ public class MainActivity extends AppCompatActivity {
         textViewArgent.setTypeface(typefaceRessource);
         textViewHomme.setTypeface(typefaceRessource);
         textViewAddRessources.setTypeface(typefaceRessource);
+
+        //ELEMENTS PROGRESSBAR
+        progressBarBonheur = (ProgressBar) findViewById(R.id.ProgressBarBonheur);
+        progressBarBonheur.setProgress(30);
+        progressBarFormation = (ProgressBar) findViewById(R.id.ProgressBarFormation);
+        progressBarFormation.setProgress(30);
+        progressBarReputation = (ProgressBar) findViewById(R.id.ProgressBarReputation);
+        progressBarReputation.setProgress(30);
+        progressBarRessources = (ProgressBar) findViewById(R.id.ProgressBarRessources);
+        progressBarRessources.setProgress(30);
+        progressBarSecurite = (ProgressBar) findViewById(R.id.ProgressBarSecurite);
+        progressBarSecurite.setProgress(30);
     }
 }
